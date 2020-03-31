27620ab41750132b6a88e82b191eb1736e98d063
JC Olivares <juancri@juancri.com>
2020-03-31 01:46:39 -0700

    Adjusting country label

--- src/util/ImageGenerator.ts
+++ src/util/ImageGenerator.ts
@@ -27,7 +27,7 @@ const COUNTRY_LABEL_FONT = {
 	anchor: 'end',
 	fill: 'black'
 };
-const COUNTRY_LABEL_OFFSET = [10, -20];
+const COUNTRY_LABEL_OFFSET = [15, -20];
 const DATE_FONT = {
 	size: 30,
 	family: 'DejaVu Sans Mono',
