91e0b68dd79a8d43a6f7af8f5929e6f8f4d26c56
JC Olivares <juancri@gmail.com>
2020-05-04 21:22:40 -0700

    Keeping scale the same for both axis

--- src/scale/DynamicScaleGenerator.ts
+++ src/scale/DynamicScaleGenerator.ts
@@ -2,22 +2,31 @@
 import * as Enumerable from 'linq';
 import { Scale, PlotSeries } from '../util/Types';
 
+const MARGIN = 0.5;
+
 export default class DynamicScaleGenerator
 {
 	public static generate(series: PlotSeries[]): Scale {
 		const lastPoints = Enumerable.from(
 			Enumerable
 				.from(series)
-				.select(serie => serie.points[serie.points.length - 1])
+				.select(serie => serie.points)
+				.where(points => points && points.length > 0)
+				.select(points => points[points.length - 1])
+				.selectMany(point => [point.x, point.y])
+				.where(x => !!x)
 				.toArray());
+
+		const min = Math.max(lastPoints.min(), 0);
+		const max = lastPoints.max();
 		return {
 			horizontal: {
-				min: lastPoints.min(p => p.x) - 0.5,
-				max: lastPoints.max(p => p.x) + 0.5
+				min: min - MARGIN,
+				max: max + MARGIN
 			},
 			vertical: {
-				min: lastPoints.min(p => p.y) - 0.5,
-				max: lastPoints.max(p => p.y) + 0.5
+				min: min - MARGIN,
+				max: max + MARGIN
 			}
 		};
 	}
