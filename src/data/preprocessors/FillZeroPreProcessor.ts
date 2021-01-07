c2959def7dfc9a65692ebe98289c3acbfac5c3d6
JC Olivares <juancri@gmail.com>
2021-01-06 17:33:56 -0800

    Adding parameters for fillZero pre-processor

--- src/data/preprocessors/FillZeroPreProcessor.ts
+++ src/data/preprocessors/FillZeroPreProcessor.ts
@@ -3,30 +3,44 @@ import * as Enumerable from 'linq';
 import { DateTime } from 'luxon';
 import { DataPoint, TimeSeries } from '../../util/Types';
 
+interface FillZeroParameters
+{
+	from?: string;
+	to?: string;
+}
+
 const DAYS_1 = { days: 1 };
 
 export default class FillZeroPreProcessor
 {
-	public static async run(series: TimeSeries[]): Promise<TimeSeries[]>
+	public static async run(series: TimeSeries[], unknownParams: unknown): Promise<TimeSeries[]>
 	{
-		return series.map(s => FillZeroPreProcessor.fillZeroSeries(s));
+		const params = unknownParams as FillZeroParameters;
+		return series.map(s => FillZeroPreProcessor.fillZeroSeries(s, params));
 	}
 
-	private static fillZeroSeries(series: TimeSeries): TimeSeries
+	private static fillZeroSeries(series: TimeSeries, params: FillZeroParameters): TimeSeries
 	{
 		if (series.data.length < 2)
 			return series;
 		const dates = Enumerable
 			.from(series.data)
 			.select(p => p.date);
-		const firstDate = dates
-			.orderBy(d => +d)
-			.first();
-		const lastDate = dates
-			.orderByDescending(d => +d)
-			.first();
+		const firstDate = FillZeroPreProcessor.getDate(
+			dates.orderBy(d => +d),
+			params?.from);
+		const lastDate = FillZeroPreProcessor.getDate(
+			dates.orderByDescending(d => +d),
+			params?.to);
 		const points = Array.from(FillZeroPreProcessor.getPoints(series.data, firstDate, lastDate));
 		return { ...series, data: points };
+	}
+
+	private static getDate(dates: Enumerable.IEnumerable<DateTime>, dateValue: string | undefined): DateTime
+	{
+		if (dateValue)
+			return DateTime.fromISO(dateValue);
+		return dates.first();
 	}
 
 	private static *getPoints(points: DataPoint[], first: DateTime, last: DateTime): Generator<DataPoint>
