4cff7d6826232e91bcffc50b4f0df40ab0725b74
JC Olivares <juancri@gmail.com>
2021-01-18 22:04:28 -0800

    Allow rename pre-processor to receive multiple rename operations

--- src/data/preprocessors/RenamePreProcessor.ts
+++ src/data/preprocessors/RenamePreProcessor.ts
@@ -10,11 +10,20 @@ export default class RenamePreProcessor
 {
 	public static async run(series: TimeSeries[], params: unknown): Promise<TimeSeries[]>
 	{
-		const renameParams = params as RenameParameters;
+		const renameParams = RenamePreProcessor.getParams(params);
 		series.forEach(s => {
-			if (renameParams.from === s.name)
-				s.name = renameParams.to;
+			const found = renameParams.find(x => x.from === s.name);
+			if (found)
+				s.name = found.to;
 		});
 		return series;
+	}
+
+	private static getParams(unknownParams: unknown): RenameParameters[]
+	{
+		const params = unknownParams as RenameParameters | RenameParameters[];
+		return Array.isArray(params) ?
+			params :
+			[params];
 	}
 }
